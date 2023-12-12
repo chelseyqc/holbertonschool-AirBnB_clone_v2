8efb568470bc51c63a88d7635431b37603d0b26a
chelseyqc <6721@holbertonstudents.com>
2023-12-12 14:39:45 +0800

    style: PEP8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -15,7 +15,7 @@ def states_list():
     displays a HTML page inside the tag BODY
     """
     states = storage.all(State).values()
-    states = sorted(states, key = lambda state: state.name)
+    states = sorted(states, key=lambda state: state.name)
     return render_template("7-states_list.html", states=states)
 
 
@@ -23,6 +23,7 @@ def states_list():
 def teardown_app(exception):
     """removes the current SQLAlchemy session"""
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
