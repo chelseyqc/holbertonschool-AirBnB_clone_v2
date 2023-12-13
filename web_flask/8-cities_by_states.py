fdfecf09a37615006f6030a99c5b2c693ce71ace
chelseyqc <6721@holbertonstudents.com>
2023-12-13 10:24:57 +0800

    feat: added a script that displays a HTML page with a list of all State obj and their cities

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """
+    displays a HTML page with a list of all State objects and cities
+    """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda state: state.name)
+    return render_template("8-cities_by_states.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown_app(exception):
+    """removes the current SQLAlchemy session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
