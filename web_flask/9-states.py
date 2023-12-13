0497db4949de0468aca6c4806a0622fbb9f2a631
chelseyqc <6721@holbertonstudents.com>
2023-12-13 16:33:06 +0800

    feat: added a script that displays State objects and cities or not found

---
+++ web_flask/9-states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route("/states", strict_slashes=False)
+@app.route("/states/<id>", strict_slashes=False)
+def states_list(id=None):
+    """display a HTML page with a list of all State objects"""
+    states = storage.all(State).values()
+    specific_state = None
+
+    if id:
+        specific_state = next((state for state in states if state.id == id),
+                              None)
+        if specific_state is not None:
+            specific_state.cities = sorted(specific_state.cities,
+                                           key=lambda city: city.name)
+        states = None
+    return render_template('9-states.html', states=states,
+                           specific_state=specific_state)
+
+
+@app.teardown_appcontext
+def teardown_app(exception):
+    """removes the current SQLAlchemy session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
