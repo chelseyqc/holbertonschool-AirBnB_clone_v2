d3e394e7ffc054b178195e3df40fc3636b096a37
chelseyqc <6721@holbertonstudents.com>
2023-12-13 10:34:09 +0800

    fix: sorting cities by name

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -15,7 +15,8 @@ def cities_by_states():
     displays a HTML page with a list of all State objects and cities
     """
     states = storage.all(State).values()
-    states = sorted(states, key=lambda state: state.name)
+    for state in states:
+        state.cities = sorted(state.cities, key=lambda city: city.name)
     return render_template("8-cities_by_states.html", states=states)
 
 
